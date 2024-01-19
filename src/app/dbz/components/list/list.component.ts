07060ccedddeee135ba9f6f522bcddc8f16b9392
jose Angel <josegallardo.cor@gmail.com>
2024-01-18 20:34:46 -0600

    funcionalidad para eliminar personaje

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Character } from '../../interfaces/character.interface';
 
 @Component({
@@ -13,5 +13,13 @@ export class ListComponent {
       name:'Thunks',
       power:10
     }
-  ]
+  ];
+  @Output()
+  public onDelete: EventEmitter<number> = new EventEmitter();
+
+  onDeleteCharacter(index: number):void {
+    // TODO: Emitir el Id del personaje
+    this.onDelete.emit(index);
+
+  }
 }
--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -22,4 +22,7 @@ export class MainPageComponent  {
   onNewCharacter(character: Character):void{
     this.characters.push(character);
   }
+  onDeleteCharacter(index:number):void {
+    this.characters.splice(index,1);
+  }
 }
