9052e4894feee4e60079b38604779264fac9383d
jose Angel <josegallardo.cor@gmail.com>
2024-01-20 22:09:09 -0600

    refactorizacion

--- src/app/dbz/pages/main-page.component.ts
+++ src/app/dbz/pages/main-page.component.ts
@@ -8,7 +8,14 @@ import { DbzService } from '../services/dbz.service';
 })
 
 export class MainPageComponent  {
-  constructor(public dbzService: DbzService){
-
+  constructor(private dbzService: DbzService){}
+  get characters(): Character[]{
+    return [... this.dbzService.characters]
+  }
+  onDeleteCharacterById(id: string):void{
+    this.dbzService.deleteCharacterById(id);
+  }
+  onNewCharacter(character:Character){
+    this.dbzService.addCharacter(character);
   }
 }
--- src/app/dbz/services/dbz.service.ts
+++ src/app/dbz/services/dbz.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { v4 as uuid } from 'uuid';
 
 @Injectable({
   providedIn: 'root'
@@ -7,20 +8,25 @@ import { Character } from '../interfaces/character.interface';
 export class DbzService {
   public characters: Character[] = [
     {
+      id:uuid(),
       name:'Krillin',
       power: 1000
     },{
+      id:uuid(),
       name:'Goku',
       power: 9500
     },{
+      id:uuid(),
       name:'Vegeta',
       power: 7000
     }
   ]
-  onNewCharacter(character: Character):void{
-    this.characters.push(character);
+  addCharacter(character: Character):void{
+    const newCharacter: Character = { id: uuid(), ...character };
+    this.characters.push(newCharacter);
   }
-  onDeleteCharacter(index:number):void {
-    this.characters.splice(index,1);
+  //onDeleteCharacter(index:number):void {
+  deleteCharacterById(id:string):void {
+    this.characters = this.characters.filter(character => character.id !== id);
   }
 }
